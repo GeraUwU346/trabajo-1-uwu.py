fb85fd2ecf2103f1a3612bd0e5cc356398061405
GeraUwU346 <156040277+GeraUwU346@users.noreply.github.com>
2024-01-15 20:58:02 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import random
+print(" hoy haremos un generador de constraseñas, oki?")
+l=["a","b","c","e","d","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+L=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+n=["0","1","2","3","4","5","6","7","8","9"]
+c=["@","<","-","_","#","$","%","&"]
+
+print(
+random.choice(l),
+random.choice(L),
+random.choice(n),
+random.choice(c),
+random.choice(l),
+random.choice(L),
+random.choice(n),
+random.choice(c),
+random.choice(l),
+random.choice(L),
+random.choice(n),
+random.choice(c),
+random.choice(l),
+random.choice(L),
+random.choice(n),
+random.choice(c)
+)
+
+
+
+
+
+
+
+
+
+
+
